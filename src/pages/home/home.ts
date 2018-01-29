9291b2ff38c4b8ea50e1bfcb063329d21fd1dc76
BJSS\Ammar.Haider <ammar.haider@bjss.com>
2018-01-29 14:07:16 +0000

    tweaks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,6 +18,7 @@ export class HomePage {
   base64Image: string;
   batteryLevel: number;
   isPlugged: boolean;
+  pluggedInConjunction: string;
 
   constructor(
     public platform: Platform,
@@ -41,6 +42,7 @@ export class HomePage {
       (status: BatteryStatusResponse) => {
         this.batteryLevel = status.level;
         this.isPlugged = status.isPlugged;
+        this.pluggedInConjunction = this.isPlugged ? '' : 'not';
       }
     )
   }
